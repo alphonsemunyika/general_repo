4bfe97d38378e4a3237b390cb608f9aaaecc5a55
amunyika <alphonsemunyika@gmail.com>
2024-03-14 16:27:52 +0200

    creating model....

---
+++ car_rental_spring/src/main/java/com/alphcode/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.alphcode.controller;
+
+import com.alphcode.dto.SignupRequest;
+import com.alphcode.dto.UserDto;
+import com.alphcode.services.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    ResponseEntity<?> signUpCustomer(@RequestBody SignupRequest signupRequest){
+        UserDto customerDto = authService.createCustomer(signupRequest);
+        if(customerDto==null)
+            return new ResponseEntity<>("customer not created", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(customerDto,HttpStatus.CREATED);
+
+    }
+}
+
